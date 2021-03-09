6ffcc0b6ff83ed8d2947390185951972238f38df
Katarzyna Winkler <kasia.winkler99@gmail.com>
2021-03-09 11:17:07 +0100

    node test

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const express = require('express');
+
+const port = 3000
+const HOST = '0.0.0.0';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(port, HOST);
+console.log(`http://${HOST}:${port}`)
+
