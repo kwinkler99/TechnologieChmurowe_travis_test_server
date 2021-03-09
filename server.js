509af92e2ebe5948971cae066a1874bbd634ecb4
Katarzyna Winkler <kasia.winkler99@gmail.com>
2021-03-09 23:20:24 +0100

    add post, put and delete request to server.js

--- server.js
+++ server.js
@@ -7,8 +7,49 @@ const HOST = '0.0.0.0';
 
 const app = express();
 
+let array = []
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send({
+        'array': array
+    });
+})
+
+app.post('/:new', (req, res) => {
+    array.push({'details': req.params.new, 'id': array.length})
+    res.send({
+        addSthNew: req.params.new
+    })
+})
+
+app.put('/:id/:update', (req, res) => {
+    const id = req.params.id
+    const update = req.params.update
+    array = array.map(item => {
+        if(item.id === parseInt(id)){
+            item.details = update
+        }
+        return item
+    })
+
+    res.send({
+        'update': update
+    })
+
+})
+
+app.delete('/:id', (req, res) => {
+    const id = req.params.id
+    array = array.filter(item => item.id != parseInt(id))
+    let index = 0
+    array = array.map(item => {
+        item.id = index
+        index =+ 1
+        return item
+    })
+    res.send({
+        'deleteId': id
+    })
 })
 
 app.listen(port, HOST);
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,7 +1,23 @@
 const expect = require('chai').expect
 const server = require('../server');
 
-describe('test', () => {
+describe('node test', () => {
+
+    describe('get test', () => {
+
+    })
+
+    describe('post test', () => {
+        
+    })
+
+    describe('put test', () => {
+        
+    })
+
+    describe('delete test', () => {
+        
+    })
     it('should reaturn a string', () => {
         expect('ci with travis').to.equal('ci with travis');
     });
